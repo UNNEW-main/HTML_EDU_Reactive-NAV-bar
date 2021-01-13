11cc8d3f9ad31151616fb0b6a064fa4bfa310d00
UNNEW-main <startori9388@naver.com>
2021-01-13 16:02:51 +0900

    주석 추가

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toggleBtn = document.querySelector('.toogleBtn')
+const toggleBtn = document.querySelector('.toogleBtn') /*toggleBtn에 클래스 .toogleBtn 정보 입력*/
 const menu = document.querySelector('.navbar__list')
 const icon = document.querySelector('.icon_item')
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => { /*toggleBtn이 클릭되었을 때 일어나는 이벤트*/
     menu.classList.toggle('active');
     icon.classList.toggle('active');
 });
